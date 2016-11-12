c768325d44a873f064b0eaa32d8df6a9dee37a95
Linda Doong <linda.doong@berkeley.edu>
2016-11-12 14:53:30 -0800

    fb login

---
+++ lfg/LFG/platforms/ios/build/LFG.build/Debug-iphonesimulator/LFG.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/main.m \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/lindadoong/Desktop/CalHacks/lfg/LFG/platforms/ios/internal/TNSDebugging.h
